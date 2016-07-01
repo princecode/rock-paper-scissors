37a7bc996a22345b7a3c88075f38eb32c1823708
Prince <princemwilliams@gmail.com>
2016-07-01 13:22:53 -0400

    RandomAI done and StubbornAI WIP.

---
+++ RockPaperScissors/RandomAI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class RandomAI : IPlayer
+    {
+        Random random = new Random();
+        public int NextMove()
+        {
+            //0 for rock, 1 for paper, 2 for scissors
+            return random.Next(0, 3);
+
+
+        }
+
+        public void SaveResult(int myMove, int otherMove)
+        {
+            // this method intentionally left blank
+        }
+
+
+    }
+}
